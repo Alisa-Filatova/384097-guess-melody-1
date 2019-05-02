ca0be8038b490d7d7cbc1a2fab650adca1c4de80
Alisa Filatova <alicefill@bk.ru>
2019-05-02 23:12:13 +0300

    Учебный проект: валидируй, как никогда не валидировал

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,8 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import WelcomeScreen from '../welcome-screen/welcome-screen.jsx';
 
-const App = () => {
-  return <WelcomeScreen />;
+const App = (props) => {
+  const {gameTime, mistakesCount} = props;
+
+  return (
+    <WelcomeScreen
+      time={gameTime}
+      mistakesCount={mistakesCount}
+    />
+  );
+};
+
+App.propTypes = {
+  gameTime: PropTypes.number.isRequired,
+  mistakesCount: PropTypes.number.isRequired,
 };
 
 export default App;
